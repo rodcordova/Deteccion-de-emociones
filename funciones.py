22d0f1f2712671f1ce2576b1062cdfcd39ed42ad
rodcordova <rodrigocordova836@gmail.com>
2023-11-10 21:25:05 -0300

    agregando las frecuencias de palabras

--- funciones.py
+++ funciones.py
@@ -2,8 +2,12 @@ import pandas as pd
 import matplotlib.pyplot as plt
 import seaborn as sns
 import numpy as np
-import warnings
 from matplotlib import cycler
+
+import nltk
+from wordcloud import WordCloud #
+from nltk.corpus import stopwords
+from nltk.tokenize import word_tokenize
 
 colors = cycler('color',
                 ['#669FEE', '#66EE91', '#9988DD',
@@ -39,3 +43,42 @@ def missing_values_table(df):
             " columns that have missing values.")
     return mis_val_table_ren_columns
 
+
+def generate_wordcloud(texts):
+    # Get English prepositions
+    stop_words = set(stopwords.words('english'))
+
+    # Combine all the texts into a single string
+    combined_text = ' '.join(str(text) for text in texts)
+
+    # Tokenize the combined text into words
+    words = word_tokenize(combined_text)
+
+    # Filter out the prepositions
+    filtered_words = [word.lower() for word in words if word.lower() not in stop_words]
+
+    # Create a frequency distribution of the words
+    freq_dist = nltk.FreqDist(filtered_words)
+
+    # Create the word cloud with word frequencies as input
+    wordcloud = WordCloud(width=800, height=400, background_color='white').generate_from_frequencies(freq_dist)
+
+    # Display the word cloud
+    plt.figure(figsize=(10, 5))
+    plt.imshow(wordcloud, interpolation='bilinear')
+    plt.axis('off')
+    plt.show()
+
+def generate_frec(texts,orden:bool):
+    combined_text = ' '.join(str(text) for text in texts)
+    words = word_tokenize(combined_text)
+    filtered_words = [word.lower() for word in words ]
+    freq_dist = nltk.FreqDist(filtered_words)
+    # pasamos a dataframe
+    df_frec=pd.DataFrame(list(freq_dist.items()),columns=['Word','Frecuencia'])
+    # Ordenamos
+    df_frec.sort_values('Frecuencia',ascending=orden,inplace=True)
+    # reiniciamos indice
+    df_frec.reset_index(drop=True,inplace=True)
+    # Graficamos
+    return df_frec
