0be330a757f952d440fd403fa371db68e69aa3db
rodcordova <rodrigocordova836@gmail.com>
2023-11-11 19:45:33 -0300

    actualizar el readme

--- pages/01_📊_visualizacion.py
+++ pages/01_📊_visualizacion.py
@@ -4,7 +4,9 @@ import pandas as pd
 import matplotlib.pyplot as plt
 import seaborn as sns
 from matplotlib import cycler
-
+import joblib
+#from sklearn.svm import LinearSVC
+from sklearn.feature_extraction.text import CountVectorizer
 
 colors = cycler('color',
                 ['#669FEE', '#66EE91', '#9988DD',
@@ -20,7 +22,7 @@ plt.rc('legend', facecolor="#313233", edgecolor="#313233")
 plt.rc("text", color="#C9C9C9")
 plt.rc('figure', facecolor='#313233')
 
-
+## HISTOGRAMA
 # Cargamos datos desde mysql
 
 my_con=create_engine('mysql+pymysql://root:root1234@localhost:3306/nlp')
@@ -60,7 +62,7 @@ with col2:
         item.set_rotation(45)
     st.pyplot(fig)
 
-# sentimiento
+## SENTIMIENTO
 query_bing_sen = text('''
     SELECT * FROM bing_btc   
 ''')
@@ -74,8 +76,38 @@ df_newsapi_sen = pd.read_sql_query(query_news_sen, my_con)
 news_valor=sum([val for val in df_newsapi_sen['sentiment_analysis']])/df_newsapi_sen.shape[0]
 
 
+
+
+# QUITARLO
+# Crear un DataFrame vacío
+df = pd.DataFrame(columns=['Texto'])
+
+
+# Recopilar texto desde el usuario
+nuevo_texto = st.text_area('Ingrese noticia que quiera analizar y presione Enter:')
+boton=st.button('Guardar Texto')
+# Verificar si se ingresó texto y si se presionó Enter
+if nuevo_texto and boton:
+    # Agregar el nuevo texto al DataFrame
+    #df = df.append({'Texto': nuevo_texto}, ignore_index=True)
+    st.success('¡Texto guardado con éxito!')
+# Cargamos el modelo
+best_model=joblib.load('best_model_sentiment.plk')
+cou_vec=joblib.load('count_vectorizer.pkl')
+
+# Realizar la predicción
+prediccion=best_model.predict(cou_vec.transform([nuevo_texto]).toarray())[0]
+#prediccion = best_model.predict(nuevo_texto.toarray())
+
+st.write("0: negativa,1: positiva,2: neutra")
 if st.checkbox('Mostrar sentimiento'):
     st.write('Bing',bing_valor)
     st.write('News',news_valor)
+    st.write('Manual',prediccion)
+
+#################################
+st.title('Series temporales')
+st.markdown('***')
+
 
 
--- streamlit.py
+++ streamlit.py
@@ -4,6 +4,6 @@ st.title('Trabajo Práctico Individual')
 
 st.markdown('***')
 st.markdown('### Detección de Emociones')
-st.markdown('### Detección de Emociones')
-st.markdown('### Detección de Emociones')
+st.markdown('### Prediccion de precio')
+st.markdown('### Prediccion con serie temporales')
 st.markdown('### Detección de Emociones')
