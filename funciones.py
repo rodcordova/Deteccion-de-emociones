ab05664e5b52dc1496850697c1956567f2678805
rodcordova <rodrigocordova836@gmail.com>
2023-11-13 20:37:42 -0300

    agrego funcionalidad para predecir el precio del bitcoin

--- funciones.py
+++ funciones.py
@@ -9,6 +9,12 @@ from wordcloud import WordCloud #
 from nltk.corpus import stopwords
 from nltk.tokenize import word_tokenize
 
+
+import streamlit as st
+import MetaTrader5 as mt5
+from datetime import datetime
+import ta
+
 colors = cycler('color',
                 ['#669FEE', '#66EE91', '#9988DD',
                  '#EECC55', '#88BB44', '#FFBBBB'])
@@ -43,7 +49,7 @@ def missing_values_table(df):
             " columns that have missing values.")
     return mis_val_table_ren_columns
 
-
+##################### ANALISIS DE ESNTIMIENTO
 def generate_wordcloud(texts):
     # Get English prepositions
     stop_words = set(stopwords.words('english'))
@@ -82,3 +88,181 @@ def generate_frec(texts,orden:bool):
     df_frec.reset_index(drop=True,inplace=True)
     # Graficamos
     return df_frec
+
+################ DIVERSIFICAR CARTERA
+# FXPRO
+server="FxPro-MT5"
+login = "5843847"
+password="bYsJsFu9"
+if  mt5.initialize(server=server, login=login, password=password)==False:
+    mt5.initialize(server=server, login=login, password=password)
+    print("Se conecto")
+
+def preprocessing_mt5(symbol, n, timeframe=mt5.TIMEFRAME_H1):
+
+    if mt5.initialize() == False:
+        mt5.initialize()
+        
+    # Current date extract
+    utc_from = datetime.now()
+    # Import the data into a tuple
+    rates = mt5.copy_rates_from(symbol, timeframe, utc_from, n)
+    # Tuple to dataframe
+    rates_frame = pd.DataFrame(rates)
+    # Convert time in seconds into the datetime format
+    rates_frame['time'] = pd.to_datetime(rates_frame['time'], unit='s')
+    # Convert the column "time" in the right format
+    rates_frame['time'] = pd.to_datetime(rates_frame['time'], format='%Y-%m-%d')
+    # Set column time as the index of the dataframe
+    rates_frame = rates_frame.set_index('time')
+
+    # Delete the two last columns
+    rates_frame = rates_frame.iloc[:,:-2]
+    # Rename
+    rates_frame.columns = ["open", "high", "low", "close", "volume"]
+    
+    # Desconectar del servidor de MetaTrader 5
+    mt5.shutdown()
+    return rates_frame
+
+## Estrategia SMAy RSI
+
+
+def support_resistance(input,n=1000,duration=5,spread=0, mt5_live=True):
+
+    if mt5_live:
+        df = preprocessing_mt5(input,n=n)
+    
+    # Support and resistance building
+    df["support"] = np.nan
+    df["resistance"] = np.nan
+
+    df.loc[(df["low"].shift(5) > df["low"].shift(4)) &
+            (df["low"].shift(4) > df["low"].shift(3)) &
+            (df["low"].shift(3) > df["low"].shift(2)) &
+            (df["low"].shift(2) > df["low"].shift(1)) &
+            (df["low"].shift(1) > df["low"].shift(0)), "support"] = df["low"]
+
+
+    df.loc[(df["high"].shift(5) < df["high"].shift(4)) &
+    (df["high"].shift(4) < df["high"].shift(3)) &
+    (df["high"].shift(3) < df["high"].shift(2)) &
+    (df["high"].shift(2) < df["high"].shift(1)) &
+    (df["high"].shift(1) < df["high"].shift(0)), "resistance"] = df["high"]
+
+    # Create Simple moving average 30 days
+    df["SMA fast"] = df["close"].rolling(30).mean()
+
+    # Create Simple moving average 60 days
+    df["SMA slow"] = df["close"].rolling(60).mean()
+
+    df["rsi"] = ta.momentum.RSIIndicator(df["close"], window=10).rsi()
+
+    # RSI yersteday
+    df["rsi yersteday"] = df["rsi"].shift(1)
+
+    # Create the signal
+    df["signal"] = 0
+
+    df["smooth resistance"] = df["resistance"].fillna(method="ffill")
+    df["smooth support"] = df["support"].fillna(method="ffill")
+
+    
+    condition_1_buy = (df["close"].shift(1) < df["smooth resistance"].shift(1)) & \
+                        (df["smooth resistance"]*(1+0.5/100) < df["close"])
+    condition_2_buy = df["SMA fast"] > df["SMA slow"]
+
+    condition_3_buy = df["rsi"] < df["rsi yersteday"]
+
+    condition_1_sell = (df["close"].shift(1) > df["smooth support"].shift(1)) & \
+                        (df["smooth support"]*(1+0.5/100) > df["close"])
+    condition_2_sell = df["SMA fast"] < df["SMA slow"]
+
+    condition_3_sell = df["rsi"] > df["rsi yersteday"]
+
+    df.loc[condition_1_buy & condition_2_buy & condition_3_buy, "signal"] = 1
+    df.loc[condition_1_sell & condition_2_sell & condition_3_sell, "signal"] = -1
+
+
+    # Calculamos las ganancias
+    df["pct"] = df["close"].pct_change(1)
+
+    df["return"] = np.array([df["pct"].shift(i) for i in range(duration)]).sum(axis=0) * (df["signal"].shift(duration))
+    df.loc[df["return"]==-1, "return"] = df["return"]-spread
+    df.loc[df["return"]==1, "return"] = df["return"]-spread
+
+    return df#["return"]
+
+def drawdown_function(serie):
+
+    # Calculamos la suma de los rendimientos
+    cum = serie.dropna().cumsum() + 1
+    
+    # Calculamos el máximo de la suma en el período (máximo acumulado) # (1,3,5,3,1) --> (1,3,5,5,5)
+    running_max = np.maximum.accumulate(cum)
+    
+    # Calculamos el drawdown
+    drawdown = cum/running_max - 1
+    return drawdown
+
+
+def BackTest(serie, annualiazed_scalar=252):
+    # Importar el benchmark
+    sp500 = preprocessing_mt5('#US500_Z23', n=7000)['close'].pct_change(1)# cambio porcentual del actual valor con el de 1 atras
+
+    # Cambiar el nombre
+    sp500.name = "SP500"
+
+    # Concatenar los retornos y el sp500
+    val = pd.concat((serie,sp500), axis=1).dropna()
+    # Calcular el drawdown
+    drawdown = drawdown_function(serie)*100
+
+    # Calcular el max drawdown
+    max_drawdown = -np.min(drawdown)
+
+    # Put a subplots
+    fig, (cum, dra) = plt.subplots(1,2, figsize=(20,6))
+
+    # Put a Suptitle
+    fig.suptitle("Backtesting", size=20)
+
+    # Returns cumsum chart
+    cum.plot(serie.cumsum()*100, color="#39B3C7")
+
+    # SP500 cumsum chart
+    cum.plot(val["SP500"].cumsum()*100, color="#B85A0F")
+
+    # Put a legend
+    cum.legend(["La prueba", "SP500"])
+
+    # Set individual title
+    cum.set_title("Cumulative Return", size=13)
+
+    cum.set_ylabel("Cumulative Return %", size=11)
+
+    # Put the drawdown
+    dra.fill_between(drawdown.index,0,drawdown, color="#C73954", alpha=0.65)
+
+    # Set individual title
+    dra.set_title("Drawdown de lo que pruebo en SMA_strategy", size=13)
+
+    dra.set_ylabel("drawdown en %", size=11)
+
+    # Plot the graph
+    #plt.show()
+    st.pyplot(fig)
+    # Calcular el índice sortino
+    sortino = np.sqrt(annualiazed_scalar) * serie.mean()/serie.loc[serie<0].std()
+
+    # Calcular el índice  beta
+    beta = np.cov(val[["return", "SP500"]].values,rowvar=False)[0][1] / np.var(val["SP500"].values)
+
+    # Calcular el índice  alpha
+    alpha = annualiazed_scalar * (serie.mean() - beta*serie.mean())
+
+      # Imprimir los estadísticos
+    st.write(f"Sortino: {np.round(sortino,3)} ##SI  >1 riesgo menor que rendimiento")
+    st.write(f"Beta: {np.round(beta,3)} ##SI  <1 menor b=> menor riesgo")
+    st.write(f"Alpha: {np.round(alpha*100,3)} % ##SI  >0 Alpha rendimiento superior al mercado")
+    st.write(f"MaxDrawdown: {np.round(max_drawdown,3)} %")
--- pages/01_📊_visualizacion.py
+++ pages/01_📊_visualizacion.py
@@ -1,12 +1,19 @@
 import streamlit as st
 from sqlalchemy import create_engine,text #se lleva bien 
 import pandas as pd
+import numpy as np
 import matplotlib.pyplot as plt
 import seaborn as sns
 from matplotlib import cycler
 import joblib
 #from sklearn.svm import LinearSVC
 from sklearn.feature_extraction.text import CountVectorizer
+from funciones import support_resistance,preprocessing_mt5,drawdown_function,BackTest
+
+## series
+from prophet.diagnostics import cross_validation
+from prophet.diagnostics import performance_metrics
+from prophet.plot import plot_cross_validation_metric
 
 colors = cycler('color',
                 ['#669FEE', '#66EE91', '#9988DD',
@@ -105,9 +112,165 @@ if st.checkbox('Mostrar sentimiento'):
     st.write('News',news_valor)
     st.write('Manual',prediccion)
 
-#################################
-st.title('Series temporales')
+################################# CARTERA
+st.title('Diversificar cartera')
+
+st.markdown('***')
+########
+# Área de entrada de texto
+lista = st.text_input("Ingresa tu lista separada por comas",'BITCOIN')
+# Convertir el texto ingresado en una lista
+listnames = [item.strip() for item in lista.split(',')]
+returns = pd.DataFrame()
+boton=st.button('Guardar activos')
+# Verificar si se ingresó texto y si se presionó Enter
+if listnames and boton:
+    # Agregar el nuevo texto al DataFrame
+    #df = df.append({'Texto': nuevo_texto}, ignore_index=True)
+    st.success('¡Texto guardado con éxito!')
+
+# Calcular la rentabilidad de cada estrategia
+for name in listnames:
+
+    returns[name] = support_resistance(name,mt5_live=True,n=7000, duration=10, spread=0.01)['return']
+
+# Representar los resultados
+cumulative_returns =returns.fillna(value=0).cumsum()*100
+# Crear un gráfico con matplotlib
+fig, ax = plt.subplots(figsize=(15, 6))
+cumulative_returns.plot(ax=ax)
+
+ax.set_title('Rendimientos Acumulativos')
+ax.set_xlabel('Período')
+ax.set_ylabel('Rendimiento Acumulativo (%)')
+st.pyplot(fig)
+
+#####
+returns["portfolio"] = returns.sum(axis=1)/returns.shape[1]
+# Dataframe vacío
+values = pd.DataFrame(index=["RETURN", "DRAWDOWN", "RETURN DRAWDOWN RATIO"])
+# Calculamos retorno/drawdown
+for col in returns.columns:
+    ret = (returns[col].dropna().cumsum().iloc[-1])
+    dd = -np.min(drawdown_function(returns[col].dropna()))
+    ret_dd = ret/dd
+    
+    values[col] = ret,dd,ret_dd
+
+cartera=values.transpose().sort_values(by="RETURN DRAWDOWN RATIO", ascending=False)
+# mostrar dataframe
+#if st.checkbox('mostrar dataframe'):
+st.dataframe(cartera)
+
+########
+# Área de entrada de texto
+cripto = st.text_input("Ingresa la criptomoneda a analizar con la estrategia", "BITCOIN")
+boton=st.button('Guardar cripto')
+# Verificar si se ingresó texto y si se presionó Enter
+if cripto and boton:
+    # Agregar el nuevo texto al DataFrame
+    #df = df.append({'Texto': nuevo_texto}, ignore_index=True)
+    st.success('¡Texto guardado con éxito!')
+
+resul=support_resistance(cripto)
+#st.hist(resul['return'])
+# Crear un histograma con Matplotlib
+fig, ax = plt.subplots(figsize=(15, 6))
+ax.hist(resul['return'], bins=30, color='pink', alpha=0.7)
+
+ax.set_title('Histograma de Retornos')
+ax.set_xlabel('Retornos')
+ax.set_ylabel('Frecuencia')
+st.pyplot(fig)
+
+strategy=support_resistance("BITCOIN",n=7000, mt5_live=True) # para año mes dia (con api) ## cartera PYPL, PFE (farmaceutica)
+dfc = strategy['return'].loc["2020":]-0.00001 # coste de transaccion .00001
+
+BackTest(dfc, 252)
+
+
+fig, ax = plt.subplots(figsize=(15, 6))
+strategy[["close", "SMA fast", "SMA slow"]].loc["2023-04-12 22:00:00":].plot(ax=ax)
+
+ax.set_title('Rendimientos Acumulativos')
+ax.set_xlabel('Período')
+ax.set_ylabel('Rendimiento Acumulativo (%)')
+st.pyplot(fig)
+
+############### SERIES TEMPORALES
+st.title('Series Temporales')
+
 st.markdown('***')
 
 
+periodo_s = st.text_input(f"Ingresa peiodo en hs del {cripto}", "24")
+boton_s=st.button('Guardar periodo')
+# Verificar si se ingresó texto y si se presionó Enter
+if periodo_s and boton_s:
+    # Agregar el nuevo texto al DataFrame
+    #df = df.append({'Texto': nuevo_texto}, ignore_index=True)
+    st.success('¡Texto guardado con éxitos!')
 
+# Cargamos el modelo
+model_serie=joblib.load('serie_temporal.pkl')
+
+future = model_serie.make_future_dataframe(periods=int(periodo_s), freq='H') #32 semanas tiene un año
+forecast = model_serie.predict(future)
+
+#model_serie.plot(forecast,xlabel='Fecha',ylabel='Fecha')
+#plt.title('Bitcoin',fontsize=13)
+#plt.show()
+
+fig, ax = plt.subplots(figsize=(15, 6))
+model_serie.plot(forecast,ax=ax)
+
+ax.set_title(f'{cripto}')
+ax.set_xlabel('Fecha')
+ax.set_ylabel('Precio')
+st.pyplot(fig)
+
+#df_cv=cross_validation(model_serie, horizon=periodo_s+" hours", initial="120 hours")
+#fig = plot_cross_validation_metric(df_cv, metric='rmse')
+#st.pyplot(fig)
+#df_p = performance_metrics(df_cv)
+#st.write('El RMSE es:', df_p.rmse.mean())
+
+#st.write(f'Precio para la fecha {} del {} es: ', )
+
+fecha = st.text_input(f"Ingresa fecha para predecir el valor de {cripto} ", "2023-11-12 04:00:00")
+boton_f=st.button('Guardar fecha')
+# Verificar si se ingresó texto y si se presionó Enter
+if fecha and boton_f:
+    # Agregar el nuevo texto al DataFrame
+    #df = df.append({'Texto': nuevo_texto}, ignore_index=True)
+    st.success('¡Texto guardado con éxitos!')
+
+# 1c) predecimos etiqueta de uba salida
+future_date = pd.to_datetime(fecha)
+future = pd.DataFrame({'ds':[future_date]})
+# predecimos
+forecast = model_serie.predict(future)
+predicted_price=forecast['yhat'].iloc[0]
+st.write(f'El precio predicho por serie temporal para {cripto} es de: ', predicted_price)
+
+######################## Prediccion
+best_pipe=joblib.load('model_best_pipeline.plk')
+
+fecha_numeros=(pd.to_datetime(fecha) - pd.Timestamp("1970-01-01")) // pd.Timedelta('1s')
+
+input_data = {
+        #"open": [openai],
+        #"high": [high],
+        #"low": [low],
+        #"volume": [volume],  # Agregar el nombre del nuevo juego
+        "fecha_numeros":[fecha_numeros]
+}
+input_df = pd.DataFrame(input_data)
+
+# Convertir el diccionario en una matriz bidimensional
+input_array = np.array(list(input_data.values())).reshape(1, -1)
+
+# Luego, puedes usar input_array para hacer la predicción
+predicted_price = best_pipe.predict(input_array)
+
+st.write(f'El precio predicho por Prediccion ML para {cripto} es de: ', predicted_price[0])
